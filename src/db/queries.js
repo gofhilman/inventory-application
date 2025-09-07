612591a131d21134813d1f659344ec42245c3bc4
Hilman Fikry <hilman12fikry@gmail.com>
2025-09-07 21:55:29 +0700

    WIP: create SQL queries

--- src/db/queries.js
+++ src/db/queries.js
@@ -7,15 +7,20 @@ async function getQueryNameList(sql) {
 
 async function getAllFilters() {
   const [categories, languages, tools] = await Promise.all([
-    getQueryNameList(`SELECT name FROM category`),
-    getQueryNameList(`SELECT name FROM language`),
-    getQueryNameList(`SELECT name FROM tool`),
+    getQueryNameList(`SELECT name FROM category;`),
+    getQueryNameList(`SELECT name FROM language;`),
+    getQueryNameList(`SELECT name FROM tool;`),
   ]);
   return { categories, languages, tools };
 }
 
 async function getFilteredProjects(category, language, tool) {
-  
+  const baseSql = `
+    SELECT project.name, description, source, image, category.name
+    FROM project
+    LEFT JOIN project_category ON project_category.project_id = project.id
+    LEFT JOIN category ON category.id = project_category.category_id
+  `;
 }
 
-module.exports = { getAllFilters };
+module.exports = { getAllFilters, getFilteredProjects };
