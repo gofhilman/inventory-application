03cc78667f45c2613225ab40162dd128ff127cf4
Hilman Fikry <hilman12fikry@gmail.com>
2025-09-14 14:58:01 +0700

    feat: create project page view

--- public/modules/main.js
+++ public/modules/main.js
@@ -2,9 +2,15 @@ import headerTyping from "/modules/header-typing.js";
 
 const typing = document.querySelector("#typing");
 const cursor = document.querySelector("#cursor");
+const deleteButton = document.querySelector("#delete");
+const deleteDialog = document.querySelector("#delete-dialog");
 
 setInterval(() => {
   headerTyping.type();
   typing.textContent = headerTyping.typedString;
   cursor.textContent = headerTyping.cursor;
 }, 120);
+
+deleteButton.addEventListener("click", deleteDialog.showModal);
+
+// TBC
